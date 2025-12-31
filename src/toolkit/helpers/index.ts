ea2f56700b5c701d3cb923ab0f41bed83553f8d3
Lenic Lei <lenic_lei@manka.co.jp>
2025-12-31 09:59:45 +0900

    refactor: Add explicit type annotations to exported constants and import necessary types.

--- src/toolkit/helpers/index.ts
+++ src/toolkit/helpers/index.ts
@@ -1,3 +1,14 @@
+import type {
+  IAccumulatedDimension,
+  ICellDimension,
+  IDataRegion,
+  IItemBoundary,
+  IItemDimension,
+  IScrollOffset,
+  ISheetConfig,
+  ISheetDimension,
+} from './types';
+
 import { combineLatest, map } from 'rxjs';
 
 import { AccumulatedDimension } from './accumulatedDimension';
@@ -9,28 +20,35 @@ import { ScrollOffset } from './scrollOffset';
 import { SheetConfig } from './sheetConfig';
 import { SheetDimension } from './sheetDimension';
 
-export const config = new SheetConfig(50000, 5000, 4, 3);
-export const columnDimension = new ItemDimension(
+export const config: ISheetConfig = new SheetConfig(50000, 5000, 4, 3);
+export const columnDimension: ItemDimension = new ItemDimension(
   combineLatest([config.minColumnWidth$, config.headerWidth$, config.columnWidth$]).pipe(
     map((v) => ({ minDimension: v[0], headerDimension: v[1], defaultDimension: v[2] })),
   ),
 );
-export const rowDimension = new ItemDimension(
+export const rowDimension: IItemDimension = new ItemDimension(
   combineLatest([config.minRowHeight$, config.headerHeight$, config.rowHeight$]).pipe(
     map((v) => ({ minDimension: v[0], headerDimension: v[1], defaultDimension: v[2] })),
   ),
 );
-export const accumulatedColumnDimension = new AccumulatedDimension(columnDimension, config.columnCount$);
-export const accumulatedRowDimension = new AccumulatedDimension(rowDimension, config.rowCount$);
-export const sheetDimension = new SheetDimension(config, accumulatedColumnDimension, accumulatedRowDimension);
-export const scrollOffset = new ScrollOffset(sheetDimension);
-export const columnBoundary = new ItemBoundary(accumulatedColumnDimension, {
+export const accumulatedColumnDimension: IAccumulatedDimension = new AccumulatedDimension(
+  columnDimension,
+  config.columnCount$,
+);
+export const accumulatedRowDimension: IAccumulatedDimension = new AccumulatedDimension(rowDimension, config.rowCount$);
+export const sheetDimension: ISheetDimension = new SheetDimension(
+  config,
+  accumulatedColumnDimension,
+  accumulatedRowDimension,
+);
+export const scrollOffset: IScrollOffset = new ScrollOffset(sheetDimension);
+export const columnBoundary: IItemBoundary = new ItemBoundary(accumulatedColumnDimension, {
   scrollValue$: scrollOffset.left$,
   frozenCount$: config.frozenColumns$,
 });
-export const rowBoundary = new ItemBoundary(accumulatedRowDimension, {
+export const rowBoundary: IItemBoundary = new ItemBoundary(accumulatedRowDimension, {
   scrollValue$: scrollOffset.top$,
   frozenCount$: config.frozenRows$,
 });
-export const cellDimension = new CellDimension(columnBoundary, rowBoundary);
-export const dataRegion = new DataRegion(config, columnBoundary, rowBoundary, sheetDimension);
+export const cellDimension: ICellDimension = new CellDimension(columnBoundary, rowBoundary);
+export const dataRegion: IDataRegion = new DataRegion(config, columnBoundary, rowBoundary, sheetDimension);
