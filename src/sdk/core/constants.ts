e514e2eb1290efb5ef29a538d449cafebdd737a4
Lenic Lei <lenic_lei@manka.co.jp>
2026-02-04 14:48:09 +0900

    feat: Add SCROLL_CONTAINER and VIRTUAL_CONTENT constants and JSDoc comments for UI-related symbols.

--- src/sdk/core/constants.ts
+++ src/sdk/core/constants.ts
@@ -1,5 +1,21 @@
 import type { TIdentifier } from '../../container';
 
+/**
+ * Konva container
+ */
 export const KONVA_CONTAINER = Symbol('KONVA_CONTAINER');
 
+/**
+ * Scroll container
+ */
+export const SCROLL_CONTAINER = Symbol('SCROLL_CONTAINER');
+
+/**
+ * Virtual content
+ */
+export const VIRTUAL_CONTENT = Symbol('VIRTUAL_CONTENT');
+
+/**
+ * UI element
+ */
 export const UIElement: TIdentifier<HTMLElement> = Symbol('UIElement');
